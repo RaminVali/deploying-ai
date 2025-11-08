da950d426e9f40e523ce4d69bb1274bd5c6b4f6f
RaminVali <60187816+RaminVali@users.noreply.github.com>
2025-11-08 10:38:24 -0500

    committing little changes to main

---
+++ 05_src/assignment_chat/app.py
@@ -0,0 +1,46 @@
+from assignment_chat.main import get_animals_chat_agent
+from langchain_core.messages import HumanMessage, AIMessage
+import gradio as gr
+from dotenv import load_dotenv
+import os
+
+from utils.logger import get_logger
+
+_logs = get_logger(__name__)
+
+llm = get_animals_chat_agent()
+
+load_dotenv('.secrets')  #When we are doing dot-env then we have to be in the right pATH, make sure your path is good. When you run the app then go to the top folder of the APP.
+
+if not os.environ.get("OPENAI_API_KEY"):
+    raise ValueError("Missing OPENAI_API_KEY environment variable")
+
+
+def animals_chat(message: str, history: list[dict]) -> str:
+    langchain_messages = []
+    n = 0
+    _logs.debug(f"History: {history}")
+    for msg in history:
+        if msg['role'] == 'user':
+            langchain_messages.append(HumanMessage(content=msg['content']))
+        elif msg['role'] == 'assistant':
+            langchain_messages.append(AIMessage(content=msg['content']))
+            n += 1
+    langchain_messages.append(HumanMessage(content=message))
+
+    state = {
+        "messages": langchain_messages,
+        "llm_calls": n
+    }
+
+    response = llm.invoke(state)
+    return response['messages'][len(response['messages']) - 1].content
+
+chat = gr.ChatInterface(
+    fn=animals_chat,
+    type="messages"
+)
+
+if __name__ == "__main__":
+    _logs.info('Starting Animals Chat App...')
+    chat.launch()
---
+++ 05_src/assignment_chat/readme.md
@@ -0,0 +1,1 @@
+Chat interface with a couple of services submitted as assignment 2 of DSI deploying AI Micro Credential
